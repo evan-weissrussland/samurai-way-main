9ee197e38eb7b646342170106ab961f5188673fa
evan-weissrussland <evanwodichka@gmail.com>
2023-05-26 20:25:57 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+export const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
 
-export default App;
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href={'#s'}>Home</a>
+            <a href={'#s'}>News Feed</a>
+            <a href={'#s'}>Messages</a>
+        </div>
+    );
+}
