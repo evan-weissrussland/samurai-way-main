0645ec7c4bde4b48d31020f14d619d84166b62b1
evan-weissrussland <evanwodichka@gmail.com>
2023-05-26 22:01:14 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import image from './group-153.svg'
+import image1 from './image1.svg'
+
 import './App.css';
 // import {Technologies} from "./Technologies";
 // import {Header} from "./Header";
@@ -8,14 +9,27 @@ export const App = () => {
     return (
         <div className={'app-wripper'}>
             <header className={'header'}>
-                <img src={image} alt={''}/>
+                <img src={image1} alt={''}/>
             </header>
             <nav className={'nav'}>
-                <div>Profile</div>
-                <div>Message</div>
+                <div>
+                    <a href={'#'}>Profile</a>
+                </div>
+                <div>
+                    <a href={'#'}>Messages</a>
+                </div>
+                <div>
+                    <a href={'#'}>News</a>
+                </div>
+                <div>
+                    <a href={'#'}>Musik</a>
+                </div>
+                <div>
+                    <a href={'#'}>Settings</a>
+                </div>
             </nav>
             <div className={'content'}>
-                Main content
+                <img src={image} alt={''}/>
             </div>
             {/*<Header />*/}
             {/*<Technologies/>*/}
