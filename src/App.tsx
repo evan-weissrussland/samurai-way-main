2145edea9dab0b0d74961c9b47fac14f88334986
evan-weissrussland <evanwodichka@gmail.com>
2023-05-26 21:26:10 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ import './App.css';
 
 export const App = () => {
     return (
-        <div>
+        <div className={'app-wripper'}>
+            <header className={'header'}>
+                <img src={'./Group 153.svg'} alt={''}/>
+            </header>
+            <nav className={'nav'}>
+                <div>Profile</div>
+                <div>Message</div>
+            </nav>
+            <div className={'content'}>
+                Main content
+            </div>
             {/*<Header />*/}
             {/*<Technologies/>*/}
         </div>
