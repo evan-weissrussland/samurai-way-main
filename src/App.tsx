4f39f19034d35b774b5152ab9ecf1fcbf69eab70
evan-weissrussland <evanwodichka@gmail.com>
2023-05-26 22:13:35 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import image1 from './image1.svg'
-
+import image2 from './image2.jpg'
 import './App.css';
 // import {Technologies} from "./Technologies";
 // import {Header} from "./Header";
@@ -13,23 +13,38 @@ export const App = () => {
             </header>
             <nav className={'nav'}>
                 <div>
-                    <a href={'#'}>Profile</a>
+                    <a href={''}>Profile</a>
                 </div>
                 <div>
-                    <a href={'#'}>Messages</a>
+                    <a href={''}>Messages</a>
                 </div>
                 <div>
-                    <a href={'#'}>News</a>
+                    <a href={''}>News</a>
                 </div>
                 <div>
-                    <a href={'#'}>Musik</a>
+                    <a href={''}>Musik</a>
                 </div>
                 <div>
-                    <a href={'#'}>Settings</a>
+                    <a href={''}>Settings</a>
                 </div>
             </nav>
             <div className={'content'}>
-                <img src={image} alt={''}/>
+                <div>
+                    <img src={image2} alt={''}/>
+                </div>
+                <div>ava+description</div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div> post 1
+                        </div>
+                        <div> post 2
+                        </div>
+                    </div>
+                </div>
             </div>
             {/*<Header />*/}
             {/*<Technologies/>*/}
