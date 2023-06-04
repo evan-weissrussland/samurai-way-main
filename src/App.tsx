a9ed8a3a646857823e7a3109547a1d8898c5ac22
“evan-weissrussland” <evanwodichka@gmail.com>
2023-06-04 17:14:50 +0300

    q

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,15 @@
 import React from 'react';
-import image1 from './image1.svg'
-import image2 from './image2.jpg'
 import './App.css';
-// import {Technologies} from "./Technologies";
-// import {Header} from "./Header";
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 export const App = () => {
     return (
         <div className={'app-wripper'}>
-            <header className={'header'}>
-                <img src={image1} alt={''}/>
-            </header>
-            <nav className={'nav'}>
-                <div>
-                    <a href={'#'}>Profile</a>
-                </div>
-                <div>
-                    <a href={''}>Messages</a>
-                </div>
-                <div>
-                    <a href={''}>News</a>
-                </div>
-                <div>
-                    <a href={''}>Music</a>
-                </div>
-                <div>
-                    <a href={''}>Settings</a>
-                </div>
-            </nav>
-            <div className={'content'}>
-                <div>
-                    <img src={image2} alt={''}/>
-                </div>
-                <div>ava+description</div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        <div> post 1
-                        </div>
-                        <div> post 2
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {/*<Header />*/}
-            {/*<Technologies/>*/}
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import image1 from '../image1.svg'
+
+export const Header = () => {
+    return (
+        <header className={'header'}>
+                <img src={image1} alt={''}/>
+            </header>
+    );
+}
