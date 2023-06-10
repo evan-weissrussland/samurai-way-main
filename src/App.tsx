1f6a0264a4000608d80d2a47756ca5d6c11d2b62
“evan-weissrussland” <evanwodichka@gmail.com>
2023-06-10 22:52:28 +0300

    q

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 export const App = () => {
     return (
-        <div className={'app-wripper'}>
-            <Header/>
-            <Navbar/>
-            <div className={'app-wripper-content'}>
-                <Dialogs/>
-                {/*   <Profile/>*/}
+        <BrowserRouter>
+            <div className={'app-wripper'}>
+                <Header/>
+                <Navbar/>
+                <div className={'app-wripper-content'}>
+                    <Route component={Dialogs}/>
+                    <Route component={Profile}/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
