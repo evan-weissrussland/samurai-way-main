a8febfc8b4a5d81b794f8061082cf52a1877ebf3
“evan-weissrussland” <evanwodichka@gmail.com>
2023-06-10 23:10:59 +0300

    q

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
+import {Music} from "./components/Music/Music";
+import {News} from "./components/News/News";
+import {Settings} from "./components/Settings/Settings";
 
 export const App = () => {
     return (
@@ -13,8 +16,11 @@ export const App = () => {
                 <Header/>
                 <Navbar/>
                 <div className={'app-wripper-content'}>
-                    <Route component={Dialogs}/>
-                    <Route component={Profile}/>
+                    <Route path={'/dialogs'} component={Dialogs}/>
+                    <Route path={'/profile'} component={Profile}/>
+                    <Route path={'/news'} component={News}/>
+                    <Route path={'/music'} component={Music}/>
+                    <Route path={'/settings'} component={Settings}/>
                 </div>
             </div>
         </BrowserRouter>
