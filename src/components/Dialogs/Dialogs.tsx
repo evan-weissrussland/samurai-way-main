48eab5b1206d76009755012b6bd3b4df68671a41
“evan-weissrussland” <evanwodichka@gmail.com>
2023-06-11 13:46:00 +0300

    q

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,21 @@ import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>Dimych</div>
+                <div className={s.dialog}>Vitoldas</div>
+                <div className={s.dialog}>Elon Musk</div>
+                <div className={s.dialog}>Bill Gates</div>
+                <div className={s.dialog}>Instasamka</div>
+                <div className={s.dialog}>Igor</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi, Gays</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Go прокидывать props</div>
+            </div>
+
         </div>
     );
 };
