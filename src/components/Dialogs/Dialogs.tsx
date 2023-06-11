26c5a8e995fa6ea67895de7a0af67d53820fc7f3
“evan-weissrussland” <evanwodichka@gmail.com>
2023-06-11 14:28:51 +0300

    q

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog}>Dimych</div>
-                <div className={s.dialog}>Vitoldas</div>
-                <div className={s.dialog}>Elon Musk</div>
-                <div className={s.dialog}>Bill Gates</div>
-                <div className={s.dialog}>Instasamka</div>
-                <div className={s.dialog}>Igor</div>
+                <div className={`${s.dialog} ${s.active}`}>
+                    <NavLink to={'/dialogs/1'}>Dimych</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/2'}>Vitoldas</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/3'}>Elon Musk</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/4'}>Bill Gates</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/5'}>Instasamka</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/6'}>Igor</NavLink>
+                </div>
             </div>
             <div className={s.messages}>
                 <div className={s.message}>Hi, Gays</div>
