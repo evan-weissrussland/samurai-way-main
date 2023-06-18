53cc0bc1c61c7a398908780909e5434e192e5684
“evan-weissrussland” <evanwodichka@gmail.com>
2023-06-18 10:44:55 +0300

    q

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,28 +2,31 @@ import React from 'react';
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+type DialogItem = {
+    id: number
+    name: string
+}
+
+const DialogItem: React.FC<DialogItem> = (props) => {
+    const {id, name} = props
+    const path = `/dialogs/${id}`
+    return (
+        <div className={`${s.dialog} ${s.active}`}>
+            <NavLink to={path}>{name}</NavLink>
+        </div>
+    )
+}
+
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={`${s.dialog} ${s.active}`}>
-                    <NavLink to={'/dialogs/1'}>Dimych</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/2'}>Vitoldas</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/3'}>Elon Musk</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/4'}>Bill Gates</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/5'}>Instasamka</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to={'/dialogs/6'}>Igor</NavLink>
-                </div>
+                <DialogItem id={1} name={'Dimych'}/>
+                <DialogItem id={2} name={'Vitoldas'}/>
+                <DialogItem id={3} name={'Elon Musk'}/>
+                <DialogItem id={4} name={'Bill Gates'}/>
+                <DialogItem id={5} name={'Instasamka'}/>
+                <DialogItem id={6} name={'Igor'}/>
             </div>
             <div className={s.messages}>
                 <div className={s.message}>Hi, Gays</div>
