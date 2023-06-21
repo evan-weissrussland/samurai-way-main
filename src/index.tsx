416ca2e7ba6ccbd8ca531ddf00563d637429f798
“evan-weissrussland” <evanwodichka@gmail.com>
2023-06-21 14:56:15 +0300

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -2,33 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
-
-//------данные для MyPosts----------
-const posts = [
-    {id: 1, message: "Hi, how are you?", likesCount: 6},
-    {id: 2, message: "It's my first post", likesCount: 3}
-]
-
-//------Данные для компоненты DialogItem в папке Dialogs-------------
-
-const dialogs = [
-    {id: 1, name: 'Dimych'},
-    {id: 2, name: 'Vitoldas'},
-    {id: 3, name: 'Elon Musk'},
-    {id: 4, name: 'Bill Gates'},
-    {id: 5, name: 'Instasamka'},
-    {id: 6, name: 'Igor'}
-]
-
-//------Данные для компоненты Message  в папке Dialogs-------------
-
-const messages = [
-    {id: 1, message: 'Hi, Guys'},
-    {id: 2, message: 'How are you?'},
-    {id: 3, message: 'Go прокидывать props'}
-]
+import {state} from './redux/state'
 
 ReactDOM.render(
-    <App dataForMyPosts={posts} dataForDialogItem={dialogs} dataForMessage={messages}/>,
+    <App dataForMyPosts={state.posts} dataForDialogItem={state.dialogs} dataForMessage={state.messages}/>,
   document.getElementById('root')
 );
---
+++ src/redux/state.ts
@@ -0,0 +1,45 @@
+type MyPostsType = {
+    id: number
+    message: string
+    likesCount: number
+}
+type DialogsItemType = {
+    id: number
+    name: string
+}
+type MessagesType = {
+    id:number
+    message:string
+}
+type GlobalState = {
+    posts: MyPostsType[]
+    dialogs: DialogsItemType[]
+    messages: MessagesType[]
+}
+
+export let state: GlobalState = {
+//------данные для MyPosts----------
+    posts: [
+        {id: 1, message: "Hi, how are you?", likesCount: 6},
+        {id: 2, message: "It's my first post", likesCount: 3}
+    ],
+
+//------Данные для компоненты DialogItem в папке Dialogs-------------
+
+    dialogs: [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Vitoldas'},
+        {id: 3, name: 'Elon Musk'},
+        {id: 4, name: 'Bill Gates'},
+        {id: 5, name: 'Instasamka'},
+        {id: 6, name: 'Igor'}
+    ],
+
+//------Данные для компоненты Message  в папке Dialogs-------------
+
+    messages: [
+        {id: 1, message: 'Hi, Guys'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Go прокидывать props'}
+    ],
+}
