dd21dd90428ace0702c871a13fa02cc163a48b11
“evan-weissrussland” <evanwodichka@gmail.com>
2023-07-07 18:44:56 +0300

    refactor

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import s from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 
@@ -26,16 +26,21 @@ export const MyPosts: React.FC<MyPosts> = (props) => {
     })
 
     // -----------конец метода map----------------
+    const newPostElement = useRef<HTMLTextAreaElement>(null)
+    const addPost = () => {
+        const el = newPostElement.current as HTMLTextAreaElement
+        alert(el.value)
+    }
 
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
