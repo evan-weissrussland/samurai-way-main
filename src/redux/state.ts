48e2223400671a2e6a39bf31115045c1add0433e
“evan-weissrussland” <evanwodichka@gmail.com>
2023-07-16 15:36:16 +0300

    refactor

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -44,10 +44,13 @@ export type StoreType = {
 type ActionAddPostOrAddMessageType = {
     type: 'ADD-MESSAGE' | 'ADD-POST'
 }
-type ActionAddTextPostType = {
+/*type ActionAddTextPostType = {
     type: 'UPDATE-NEW-POST-TEXT'
     newPostText: string
-}
+}*/
+//аналог типизации
+type ActionAddTextPostType = ReturnType<typeof updateNewPostTextAC>
+
 type ActionAddTextMessageType = {
     type: 'UPDATE-NEW-MESSAGE-TEXT'
     newMessageText: string
@@ -122,10 +125,16 @@ export const store: StoreType = {
 
 export const addPostAC = (): ActionAddPostOrAddMessageType => ({type: ADD_POST})
 
-export const updateNewPostTextAC = (text: string): ActionAddTextPostType => ({
+/*export const updateNewPostTextAC = (text: string): ActionAddTextPostType => ({
     type: UPDATE_NEW_POST_TEXT,
     newPostText: text
-})
+})*/
+//т.к. мы используем типизацию  ActionAddTextPostType через ReturnType<typeof updateNewPostTextAC>, то в функции после
+// скобок (text: string) типизацию не ставим, но добавляем после функции инструкцию "as const"
+export const updateNewPostTextAC = (text: string) => ({
+    type: UPDATE_NEW_POST_TEXT,
+    newPostText: text
+}) as const
 
 export const addMessageAC = (): ActionAddPostOrAddMessageType => ({type: ADD_MESSAGE})
 
