9387ece76e0730bb13c159b45f7f9ea796273753
“evan-weissrussland” <evanwodichka@gmail.com>
2023-07-22 13:54:38 +0300

    refactor

--- src/redux/profile-reducer.test.ts
+++ src/redux/profile-reducer.test.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 import {GlobalStateType, ProfilePageType} from "./state";
-import {addPostAC, profileReducer} from "./profile-reducer";
+import {addPostAC, profileReducer, updateNewPostTextAC} from "./profile-reducer";
 
 test('correct profile-reducer add post', () => {
    const  profilePage:ProfilePageType = {
@@ -19,4 +19,20 @@ test('correct profile-reducer add post', () => {
     expect(endState.posts[0].id).toBe(1);
     expect(endState.posts[1].id).toBe(2);
     expect(endState.newPostText).toBe('');
+});
+
+test('correct profile-reducer add new post text', () => {
+    const  profilePage:ProfilePageType = {
+        //------данные для MyPosts в папке Profile----------
+        posts: [
+            {id: 1, message: "Hi, how are you?", likesCount: 6},
+            {id: 2, message: "It's my first post", likesCount: 3}
+        ],
+        newPostText: "ffff",
+    }
+const text = 'Vetal'
+    const endState = profileReducer(profilePage, updateNewPostTextAC(text))
+
+    expect(endState.posts.length).toBe(2);
+    expect(endState.newPostText).toBe('Vetal');
 });
