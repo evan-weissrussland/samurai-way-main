0027ebd8f92e4eac5a7ad7ae8bdd95e851f4decd
“evan-weissrussland” <evanwodichka@gmail.com>
2023-07-29 17:07:07 +0300

    refactor

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,3 +1,12 @@
-import {createStore} from "redux";
+import {combineReducers, createStore} from "redux";
+import {profileReducer} from "./profile-reducer";
+import {dialogsReducer} from "./dialogs-reducer";
+import {sidebarReducer} from "./sidebar-reducer";
 
-// const store = createStore()
+const rootReducer = combineReducers({
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebar:sidebarReducer
+})
+export const store = createStore(rootReducer);
+export type AppRootStateType = ReturnType<typeof rootReducer>
