10d961db6bf352d5cca3ee420576ad5c838d6e62
“evan-weissrussland” <evanwodichka@gmail.com>
2023-09-23 14:58:14 +0300

    refactor

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,16 +2,21 @@ import axios from "axios";
 
 
 export function getUsers(currentPage: number, pageSize: number) {
-   return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {withCredentials: true}).then(response =>
-      response.data)
+    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {withCredentials: true}).then(response =>
+        response.data)
 }
 
 export function onFollowUser(userId: number) {
-   return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, null, {withCredentials: true}).then(response =>
-       response.data)
+    return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, null, {withCredentials: true}).then(response =>
+        response.data)
 }
 
 export function onUnfollowUser(userId: number) {
-   return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, null, {withCredentials: true}).then(response =>
-       response.data)
+    return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, null, {withCredentials: true}).then(response =>
+        response.data)
+}
+
+export function authMe() {
+    return axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true}).then(response =>
+        response.data)
 }
--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -1,23 +1,25 @@
 import React from "react";
-import axios from "axios";
 import {connect} from "react-redux";
 import {setUserData} from "../../redux/auth-reducer";
 import {Header} from "./Header";
 import {AppRootStateType} from "../../redux/redux-store";
+import {authMe} from "../../api/api";
 
 export class HeaderAPIContainer extends React.Component<any, any> {
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {withCredentials: true}).then(response => {
-            response.data.resultCode === 0 && this.props.setUserData(response.data.data)
+        authMe().then(data => {
+            data.resultCode === 0 && this.props.setUserData(data)
         })
     }
+
     render() {
         return <Header isAuth={this.props.isAuth} login={this.props.login}/>
     }
 }
+
 type MapStateToPropsType = {
     isAuth: boolean
-    login:string
+    login: string
 }
 
 const mapStateToProps = (state: AppRootStateType): MapStateToPropsType => {
@@ -28,4 +30,5 @@ const mapStateToProps = (state: AppRootStateType): MapStateToPropsType => {
 }
 
 export const HeaderContainer = connect(mapStateToProps, {
-    setUserData})(HeaderAPIContainer)
+    setUserData
+})(HeaderAPIContainer)
