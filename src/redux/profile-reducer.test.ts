55dcec4bd396d82bfb7608722b837fd879600058
“evan-weissrussland” <evanwodichka@gmail.com>
2023-11-12 20:46:18 +0300

    refactor

--- src/redux/profile-reducer.test.ts
+++ src/redux/profile-reducer.test.ts
@@ -1,4 +1,11 @@
-import {addPostAC, ProfilePageType, profileReducer} from "./profile-reducer";
+import {
+    addPostAC,
+    ProfilePageType,
+    profileReducer,
+    ProfileType,
+    setUserProfileAC,
+    updateUserStatusAC
+} from "./profile-reducer";
 
 test('correct profile-reducer add post', () => {
     const profilePage: ProfilePageType = {
@@ -14,25 +21,64 @@ test('correct profile-reducer add post', () => {
     const endState = profileReducer(profilePage, addPostAC(';'))
 
     expect(endState.posts.length).toBe(3);
-    expect(endState.posts[2].id).toBe(3);
-    expect(endState.posts[0].id).toBe(1);
-    expect(endState.posts[1].id).toBe(2);
+    expect(endState.posts[0].id).toBe(3);
+    expect(endState.posts[1].id).toBe(1);
+    expect(endState.posts[2].id).toBe(2);
 });
-//
-// test('correct profile-reducer add new post text', () => {
-//     const profilePage: ProfilePageType = {
-//         //------данные для MyPosts в папке Profile----------
-//         posts: [
-//             {id: 1, message: "Hi, how are you?", likesCount: 6},
-//             {id: 2, message: "It's my first post", likesCount: 3}
-//         ],
-//         newPostText: "ffff",
-//         profile: null,
-//         status:''
-//     }
-//     const text = 'Vetal'
-//     const endState = profileReducer(profilePage, updateNewPostTextAC(text))
-//
-//     expect(endState.posts.length).toBe(2);
-//     expect(endState.newPostText).toBe('Vetal');
-// });
+
+test('correct profile-reducer add new post text', () => {
+    const profilePage: ProfilePageType = {
+        //------данные для MyPosts в папке Profile----------
+        posts: [
+            {id: 1, message: "Hi, how are you?", likesCount: 6},
+            {id: 2, message: "It's my first post", likesCount: 3}
+        ],
+        profile: null,
+        status:''
+    }
+    const text = 'Vetal'
+    const endState = profileReducer(profilePage, updateUserStatusAC(text))
+
+    expect(endState.status).toBe('Vetal');
+});
+
+
+test('correct profile-reducer add new profile', () => {
+
+    const Profile = {
+        aboutMe: 'asdf',
+        userId: 123,
+        lookingForAJob: false,
+        lookingForAJobDescription: 'yes',
+        fullName: 'Aparysh',
+        contacts: {
+            github: '',
+            vk: '',
+            facebook: '',
+            instagram: 'qwerty',
+            twitter: '',
+            website: '',
+            youtube: '',
+            mainLink: ''
+        },
+        photos: {
+            small: null,
+            large: null
+        }
+    }
+    const profilePage: ProfilePageType = {
+        //------данные для MyPosts в папке Profile----------
+        posts: [
+            {id: 1, message: "Hi, how are you?", likesCount: 6},
+            {id: 2, message: "It's my first post", likesCount: 3}
+        ],
+        profile: null,
+        status:''
+    }
+
+    const endState = profileReducer(profilePage, setUserProfileAC(Profile))
+
+    expect(endState.profile?.userId).toBe(123);
+    expect(endState.profile?.fullName).toBe('Aparysh');
+    expect(endState.profile?.contacts.instagram).toBe('qwerty');
+});
