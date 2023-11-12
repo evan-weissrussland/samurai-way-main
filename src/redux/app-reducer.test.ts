b6f51349cd4e9722134e689a734bedc4d382bad7
“evan-weissrussland” <evanwodichka@gmail.com>
2023-11-12 21:30:01 +0300

    refactor

---
+++ src/redux/app-reducer.test.ts
@@ -0,0 +1,11 @@
+
+import {addMessageAC, DialogsPageType, dialogsReducer} from "./dialogs-reducer";
+import {appReducer, initializedSuccessAC} from "./app-reducer";
+
+test('correct app-reducer inizialized app', () => {
+    const initialState = {
+        isInizialized: false
+    }
+    const endState = appReducer(initialState, initializedSuccessAC())
+    expect(endState.isInizialized).toBe(true);
+});
