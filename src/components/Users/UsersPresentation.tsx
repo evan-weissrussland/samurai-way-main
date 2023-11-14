5c74398b8267249edb8ca38db8ce7a79db57c302
“evan-weissrussland” <evanwodichka@gmail.com>
2023-11-14 18:45:33 +0300

    refactor

--- src/components/Users/UsersPresentation.tsx
+++ src/components/Users/UsersPresentation.tsx
@@ -1,9 +1,7 @@
 import React, {FC} from "react";
-import s from "./Users.module.css";
 import {UsersType} from "../../redux/users-reducer";
-import defaultAvaUser from "../../images/avauser.jpg";
-import {NavLink} from "react-router-dom";
 import {Paginator} from "../common/Paginator/Paginator";
+import {User} from "./User/User";
 
 
 type UsersPresentationType = {
@@ -33,32 +31,11 @@ export const UsersPresentation: FC<UsersPresentationType> = (
         <Paginator onPageChanged={onPageChanged} currentPage={currentPage}
                    totalUsersCount={totalUsersCount} pageSize={pageSize}/>
         {users.map((u: UsersType) => {
-            return <div key={u.id} className={s.usersContainer}>
-            <span>
-                <div>
-                   <NavLink to={`/profile/${u.id}`}>
-                    <img className={s.img} src={u.photos.small !== null ? u.photos.small : defaultAvaUser} alt="ava"/>
-                    </NavLink>
-                </div>
-                <div>
-                    {u.followed
-                        ? <button disabled={followingArray.some(id => id === u.id)}
-                                  onClick={() => onUnfollowUser(u.id)}>unfollow</button>
-                        : <button disabled={followingArray.some(id => id === u.id)}
-                                  onClick={() => onFollowUser(u.id)}>follow</button>}
-                </div>
-            </span>
-                <span>
-                <span>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                </span>
-                <span>
-                    <div>{'u.location.country'}</div>
-                    <div>{'u.location.city'}</div>
-                </span>
-            </span>
-            </div>
+            return <User key={u.id}
+                         user={u}
+                         onFollowUser={onFollowUser}
+                         onUnfollowUser={onUnfollowUser}
+                         followingArray={followingArray}/>
         })}
     </div>
 }
